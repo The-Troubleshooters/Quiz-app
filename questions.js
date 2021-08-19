0f5888004abc991b3fb17a54cb2a322497b2c37b
JeffinGBenny <jeffingbenny@gmail.com>
2021-08-19 19:47:15 +0530

    last push

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who developed python?",
+    answer: "Guido van Rossum",
+    options: [
+      "Bjarne Stroustrup",
+      "Guido van Rossum",
+      "Brendan Eich",
+      "Dennis Ritchie"
+    ]
+  },
+    {
+    numb: 2,
+    question: "1 MegaByte(Mb)= ",
+    answer: "1,024 Kb",
+    options: [
+      "1,024 Kb",
+      "1,000 Gb",
+      "1,024 Tb",
+      "1,000 Kb"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which company developed Azure?",
+    answer: "Microsoft",
+    options: [
+      "Amazon",
+      "Google",
+      "Microsoft",
+      "Facebook"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which company developed React?",
+    answer: "Facebook",
+    options: [
+      "Amazon",
+      "Google",
+      "Microsoft",
+      "Facebook"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which company developed Flutter?",
+    answer: "Google",
+    options: [
+      "Facebook",
+      "Google",
+      "Amazon",
+      "Microsoft"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
